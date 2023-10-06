7631cf4804ea818393cc8369c869718af596aec6
ad-nicolas <duquenicolas391@gmail.com>
2023-10-06 07:50:58 -0500

    Primer commit

---
+++ procesadorArchivos/src/main/java/com/workshop/procesadorArchivos/ProcesadorArchivosApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.procesadorArchivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesadorArchivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesadorArchivosApplication.class, args);
+	}
+
+}
---
+++ procesadorArchivos/src/test/java/com/workshop/procesadorArchivos/ProcesadorArchivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.procesadorArchivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesadorArchivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
